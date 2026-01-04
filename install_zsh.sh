0480f2c68ed8bb323c543e2c4c93cd2858605683
nrhevu <thevuu2001@gmail.com>
2026-01-04 23:32:37 +0700

    add nvim and highlight to zsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -50,4 +50,6 @@ fi
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+sed -i.bak 's/^plugins=(git)$/plugins=(git zsh-autosuggestions zsh-syntax-highlighting docker)/' ~/.zshrc
+
 source ~/.zshrc
